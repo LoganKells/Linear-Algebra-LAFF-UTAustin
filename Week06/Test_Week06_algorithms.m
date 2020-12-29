806e756944edd8b033337dbc59ef30eaa7c0793d
technetium-43 <58450924+technetium-43@users.noreply.github.com>
2020-12-28 19:43:15 -0600

    Moved Chapter 7 (week 07) algorithms to Week07 folder

--- Week06/Test_Week06_algorithms.m
+++ Week06/Test_Week06_algorithms.m
@@ -187,36 +187,3 @@ fprintf("Check that A * x = b.\n");
 test = b - A * x;
 display(test);
 fprintf("*********************************************************\n\n");
-
-fprintf("*********************************************************\n");
-fprintf("LAFF Homework 7.2.1.7\n");
-
-fprintf("Create a unit lower triangular matrixmatrix.\n"); 
-% (This matrix was carefully chosen so that only integers
-% are encountered during the process.)
-L = [
-     1     0     0     0
-    -1     1     0     0
-     2     1     1     0
-    -2    -1     1     1
-];
-display(L);
-
-fprintf("Create a right-hand side.  We are going to solve L * x = b for x.\n");
-b = [
-     2
-     2
-    11
-    -3
-];
-display(b);
-
-fprintf("Solve L * x = b for vector x.\n");
-x = Ltrsv_unb_var2( L, b );
-display(x);
-
-fprintf("Check that L * x = b.\n");
-
-test = b - L * x;
-display(test);
-fprintf("*********************************************************\n\n");
